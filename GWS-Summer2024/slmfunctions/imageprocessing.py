cb162d70276bd053fadd8e4a68445270e07448a4
nsong03 <85597680+nsong03@users.noreply.github.com>
2024-06-26 15:17:26 -0700

    qwer

--- GWS-Summer2024/slmfunctions/imageprocessing.py
+++ GWS-Summer2024/slmfunctions/imageprocessing.py
@@ -1,196 +1,80 @@
 from .dependencies import *
 from .settings import *
+from .simulation import *
+
+def initSLM():
+    return slmclass
+
+def initCamera():
+    return cameraclass
 
 
-def identify_tweezers(cameraimage, minintensity):
-    ## This code assumes that the camera image is positioned s.t. the 0th order diffraction is centered vertically and just out of screen
-    binary_camera_image = (cameraimage > minintensity).astype(np.uint8)
-    labeled_camera_image, num_labels_camera = label(binary_camera_image)
-    centers_camera = np.array(center_of_mass(binary_camera_image, labeled_camera_image, range(1, num_labels_camera + 1)))
-    sorted_centers = centers_camera[np.lexsort((centers_camera[:, 1], centers_camera[:, 0]))]
 
 
-    center = np.median(sorted_centers, axis=0)
-    distances = np.linalg.norm(sorted_centers - center, axis=1)
-    indices = np.argsort(distances)[-4:]
-    corners = sorted_centers[indices]
+def camerafeedback_intensityuniformity(feedbackpencost, inputphase, target_im, slmobject, cameraobject):
+    """All inputs are assumed to be of the same dimensionality, 1300 by 1300. Note that magnification adds on to the target, so
+    if target is already 3900 by 3900 magnification of 2 will make the simulation space much(!) larger. Beamtypes available are Gaussian or Constant."""
+    # Remember, the calculation region is only numpixels by numpixels
+    targetintensity = targetintensity.copy()
+    targetintensity = targetintensity / cp.max(targetintensity)
+    # Just in case we're using a highly precise target (so not delta function)
+    targetmagnification = cp.shape(targetintensity)[0] // numpixels
+    targetintensity = expand(targetintensity, magnification)
+    magnification = targetmagnification * magnification
+    slmphase = set_circlemask(expand(initialphase, magnification), numpixels *magnification)
+    inputbeam = set_circlemask(createbeam(beamtype, numpixels * magnification, sigma, mu), numpixels * magnification)
+    slmplane = join_phase_ampl(slmphase, inputbeam)
     
-    # perimeter = ConvexHull(sorted_centers) 
-    # corners = sorted_centers[perimeter.vertices]
-    return corners[np.argsort(corners[:,0])], center
-
-from scipy.ndimage import affine_transform
-
-def rotate_image(image, angle):
-    # Calculate the rotation matrix
-    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
-                                [np.sin(angle), np.cos(angle)]])
-
-    # Calculate the translation needed to reposition the rotated image
-    center = np.array(image.shape) // 2
-    translation = center - np.dot(rotation_matrix, center)
-
-    # Define the affine transformation matrix
-    transformation_matrix = np.vstack([np.column_stack([rotation_matrix, translation]),
-                                       [0, 0, 1]])
-
-    # Apply the affine transformation to rotate the image
-    rotated_image = affine_transform(image, transformation_matrix, mode='constant', cval=0)
-
-    return rotated_image
-
-from scipy.ndimage import zoom
-
-def match_images(cameraimage, targetimage, intensity):
-    camera_vertices = identify_tweezers(cameraimage, intensity)[0]
-    angle = math.atan((camera_vertices[1,0]-camera_vertices[0,0])/(camera_vertices[1,1]-camera_vertices[0,1]))
-    rotated_cameraimage = rotate_image(cameraimage, -angle)
-    rotated_cameraimage_vertices = identify_tweezers(rotated_cameraimage, intensity)[0]
-    targetimage_vertices, targetimage_center = identify_tweezers(targetimage, 1)
-
-    height_target_rect = np.abs(targetimage_vertices[0,0] - targetimage_vertices[2,0])
-    width_target_rect = np.abs(targetimage_vertices[0,1] - targetimage_vertices[1,1])
-    height_camera_rect = np.abs(rotated_cameraimage_vertices[0,0] - rotated_cameraimage_vertices[2,0])
-    width_camera_rect = np.abs(rotated_cameraimage_vertices[0,1] - rotated_cameraimage_vertices[1,1])
-
-    height_scaling = height_target_rect / height_camera_rect
-    width_scaling = width_target_rect / width_camera_rect
- 
-    scaled_camera_img = zoom(rotated_cameraimage, (height_scaling,width_scaling), mode='constant', cval = 0)
-    scaled_camera_img_vertices, scaled_camera_center = identify_tweezers(scaled_camera_img, intensity)
- 
+    weights=cp.ones((numpixels * magnification, numpixels*magnification))
+    weights_previous = targetintensity.copy()
     
-    final_camera_img = np.zeros_like(targetimage)
+    # stdinttracker = [] # For use in error calculations
+    tweezerlocation = cp.where(targetintensity == 1)
+    err_maxmindiff = []
+    err_uniformity = []
+    err_powereff = []
+
+    for _ in range(iterations):
+        startingpower = cp.sum(cp.abs(slmplane)**2)
+        fourierplane = cp.fft.fftshift(cp.fft.fft2(cp.fft.fftshift(slmplane), norm="ortho"))
+        fourierintensity = cp.square(cp.abs(fourierplane))
+        stdint = cp.divide(fourierintensity, cp.max(fourierintensity))
+
+        err_maxmindiff.append(Err_MaxMinDiff(stdint, tweezerlocation))
+        err_uniformity.append(Err_Uniformity(stdint, targetintensity))
+        err_powereff.append(Err_PowerEff(stdint, tweezerlocation))
+
+        weights = costfunction(weights, weights_previous, targetintensity, stdint, harmonicremoval, badharmonics_pixelcoords)
+        weights_previous = weights.copy()
+        ## This might be a bit confusing, but weights is now the amplitude and we re-combine it with the phase to get the next iteration
+        fourierangle = cp.angle(fourierplane)
+        fourierplane = join_phase_ampl(fourierangle, weights)
+        slmplane = cp.fft.ifftshift(cp.fft.ifft2(cp.fft.ifftshift(fourierplane), norm="ortho"))     
+        endingpower = cp.sum(cp.abs(slmplane)**2)
+        slmplane = cp.multiply(cp.divide(slmplane, endingpower), startingpower)
+        slmplane_numpixels = slmplane.copy()
+        slmplane_numpixels = cp.mean(slmplane_numpixels.reshape(numpixels, magnification, numpixels, magnification), axis=(-3,-1))
+        
+        slmphase = undiscretize_phase(discretize_phase(set_circlemask(cp.angle(slmplane_numpixels), numpixels)))
+        readout_slmphase = slmphase.copy()
+        slmplane = join_phase_ampl(expand(slmphase, magnification), inputbeam)
     
-    for y in range(np.shape(scaled_camera_img)[0]):
-        for x in range(np.shape(scaled_camera_img)[1]):
-            final_camera_img[y+(targetimage_center[0]-scaled_camera_center[0]).astype(np.int),x+(targetimage_center[1]-scaled_camera_center[1]).astype(np.int)] = scaled_camera_img[y,x]
-    return final_camera_img, -angle, height_scaling, width_scaling, (targetimage_center[0]-scaled_camera_center[0]).astype(np.int), (targetimage_center[1]-scaled_camera_center[1]).astype(np.int)
+    errors = [err_maxmindiff, err_uniformity, err_powereff]
+    labels = ["MaxMinDiff","Uniformity", "Power Efficiency"]
 
-
-def match_images_local(cameraimage, targetimage, params, scanradius):
-    cameraimage = cameraimage
-    targetimage = targetimage
-    angle, Xmag, Ymag, Xshift, Yshift = params
-    slave9 = rotate_image(cameraimage, angle)
-    slave8 = scaleimg(slave9, Xmag, Ymag)
-    master = np.zeros(np.shape(targetimage))
-    original = shiftimage(slave8, targetimage, Xshift, Yshift)
-    error = np.abs(np.sum(targetimage-original))
-    for i in range(scanradius):
-        for j in range(scanradius):
-            slave_shifted = shiftimage(slave8, targetimage, Xshift - scanradius/2 + i, Yshift - scanradius/2 +j)
-            errortemp = np.abs(np.sum(slave_shifted-targetimage))
-            if errortemp < error:
-                error = errortemp
-                master = slave_shifted
-    if np.sum(master) == 0:
-        return original
-    else:
-        return master
-
-def shiftimage(child1, parent, x_shift, y_shift):
-    child_shape = np.shape(child1)
-    parent_shape = np.shape(parent)
-    fosterparent = np.zeros(parent_shape)
-    x_shift = x_shift.astype(np.int)
-    y_shift = y_shift.astype(np.int)
-    fosterparent[x_shift:x_shift+child_shape[0], y_shift:y_shift+child_shape[1]] = child1
-    return fosterparent
-
-def scaleimg(child2, x_scaling, y_scaling):
-    scaled_child = zoom(child2, (x_scaling, y_scaling), mode='constant', cval = 0)
-    return scaled_child
-
+    readout = OptimizedOutput()
+    readout.set_all(readout_slmphase, stdint, errors, labels)
     
-    
-def objectivefunction(params, child, parent):
-    angle, Xmag, Ymag, Xshift, Yshift = params
-    slave = rotate_image(child, angle)
-    slave = scaleimg(slave, Xmag, Ymag)
-    slave = shiftimage(slave, parent, Xshift, Yshift)
-    
-    error = np.abs(np.sum(slave-parent))
-    return error
+    return readout
 
 
-def find_weighted_center(image):
-    # Create coordinate grids
-    image = norm(image)
-    image[image<0.3] = 0
-    
-    non_zero_coords = np.transpose(np.nonzero(image))
-    if non_zero_coords.shape[0] > 0:
-        average_coordinates = np.mean(non_zero_coords, axis=0)
-        return tuple(average_coordinates.astype(int))
-    else:
-        return None
 
-def tiff_to_bmp(input_path, output_path):
-    # Open TIFF image
-    tiff_image = Image.open(input_path)
 
-    # Save as BMP
-    tiff_image.save(output_path, 'BMP')
-    
-def match_and_paste(parent, child):
-    # Convert images to grayscale
-    parent_gray = cv2.cvtColor(parent, cv2.COLOR_BGR2GRAY)
-    child_gray = cv2.cvtColor(child, cv2.COLOR_BGR2GRAY)
 
-    # Use ORB (Oriented FAST and Rotated BRIEF) to find keypoints and descriptors
-    orb = cv2.ORB_create()
-    kp1, des1 = orb.detectAndCompute(parent_gray, None)
-    kp2, des2 = orb.detectAndCompute(child_gray, None)
 
-    # Use BFMatcher to find the best matches between the descriptors
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1, des2)
 
-    # Sort the matches based on their distances
-    matches = sorted(matches, key=lambda x: x.distance)
 
-    # Get the best match
-    best_match = matches[0]
 
-    # Get the keypoint coordinates for the parent and child
-    parent_pts = np.float32([kp1[best_match.queryIdx].pt])
-    child_pts = np.float32([kp2[best_match.trainIdx].pt])
 
-    # Find the transformation matrix (rotation and scaling)
-    M, _ = cv2.estimateAffinePartial2D(child_pts, parent_pts)
 
-    # Apply the transformation to the child image
-    child_transformed = cv2.warpAffine(child, M, (parent.shape[0], parent.shape[1]))
 
-    return child_transformed
-
-def identify_corners(cameraimage, targetimage, intensity):
-    camera_vertices = identify_tweezers(cameraimage, intensity)[0]
-    angle = math.atan((camera_vertices[1,0]-camera_vertices[0,0])/(camera_vertices[1,1]-camera_vertices[0,1]))
-    rotated_cameraimage = rotate_image(cameraimage, -angle)
-    rotated_cameraimage_vertices = identify_tweezers(rotated_cameraimage, intensity)[0]
-    targetimage_vertices, targetimage_center = identify_tweezers(targetimage, 1)
-
-    height_target_rect = np.abs(targetimage_vertices[0,0] - targetimage_vertices[2,0])
-    width_target_rect = np.abs(targetimage_vertices[0,1] - targetimage_vertices[1,1])
-    height_camera_rect = np.abs(rotated_cameraimage_vertices[0,0] - rotated_cameraimage_vertices[2,0])
-    width_camera_rect = np.abs(rotated_cameraimage_vertices[0,1] - rotated_cameraimage_vertices[1,1])
-
-    height_scaling = height_target_rect / height_camera_rect
-    width_scaling = width_target_rect / width_camera_rect
- 
-    scaled_camera_img = zoom(rotated_cameraimage, (height_scaling,width_scaling), mode='constant', cval = 0)
-    scaled_camera_img_vertices, scaled_camera_center = identify_tweezers(scaled_camera_img, intensity)
- 
-    
-    corners_cameraimg = np.zeros_like(targetimage)
-    # corners_cameraimg[int(targetimage_vertices[0,0]), int(targetimage_vertices[0,1])] = 255
-    # corners_cameraimg[int(targetimage_vertices[1,0]), int(targetimage_vertices[1,1])] = 255
-    # corners_cameraimg[int(targetimage_vertices[2,0]),int(targetimage_vertices[2,1])] = 255
-    # corners_cameraimg[int(targetimage_vertices[3,0]),int(targetimage_vertices[3,1])] = 255
-    corners_cameraimg[targetimage_vertices[0,0].astype(np.int), targetimage_vertices[0,1].astype(np.int)] = 255
-    corners_cameraimg[targetimage_vertices[1,0].astype(np.int), targetimage_vertices[1,1].astype(np.int)] = 255
-    corners_cameraimg[targetimage_vertices[2,0].astype(np.int),targetimage_vertices[2,1].astype(np.int)] = 255
-    corners_cameraimg[targetimage_vertices[3,0].astype(np.int),targetimage_vertices[3,1].astype(np.int)] = 255
-    return corners_cameraimg, -angle, height_scaling, width_scaling, (targetimage_center[0]-scaled_camera_center[0]).astype(np.int), (targetimage_center[1]-scaled_camera_center[1]).astype(np.int)
--- GWS-Summer2024/slmfunctions/simulation.py
+++ GWS-Summer2024/slmfunctions/simulation.py
@@ -418,7 +418,7 @@ def derivephase(costfunction, targetintensity, initialphase, iterations, magnifi
     inputbeam = set_circlemask(createbeam(beamtype, numpixels * magnification, sigma, mu), numpixels * magnification)
     slmplane = join_phase_ampl(slmphase, inputbeam)
     
-    weights=cp.ones((numpixels * magnification, numpixels*magnification))
+    weights=cp.zeros((numpixels * magnification, numpixels*magnification))
     weights_previous = targetintensity.copy()
     
     # stdinttracker = [] # For use in error calculations
@@ -474,7 +474,7 @@ def derivephase_fixed(costfunction, targetintensity, initialphase, iterations1, 
     inputbeam = set_circlemask(createbeam(beamtype, numpixels * magnification, sigma, mu), numpixels * magnification)
     slmplane = join_phase_ampl(slmphase, inputbeam)
     
-    weights=cp.ones((numpixels * magnification, numpixels*magnification))
+    weights=cp.zeros((numpixels * magnification, numpixels*magnification))
     weights_previous = targetintensity.copy()
     
     # stdinttracker = [] # For use in error calculations
@@ -534,62 +534,6 @@ def derivephase_fixed(costfunction, targetintensity, initialphase, iterations1, 
         slmphase = undiscretize_phase(discretize_phase(set_circlemask(cp.angle(slmplane_numpixels), numpixels)))
         readout_slmphase = slmphase.copy()
         slmplane = join_phase_ampl(expand(slmphase, magnification), inputbeam)
-    errors = [err_maxmindiff, err_uniformity, err_powereff]
-    labels = ["MaxMinDiff","Uniformity", "Power Efficiency"]
-
-    readout = OptimizedOutput()
-    readout.set_all(readout_slmphase, stdint, errors, labels)
-    
-    return readout
-
-def camerafeedback_intensityuniformity(feedbackpencost, inputphase, target_im, slmobject, cameraobject):
-    """All inputs are assumed to be of the same dimensionality, 1300 by 1300. Note that magnification adds on to the target, so
-    if target is already 3900 by 3900 magnification of 2 will make the simulation space much(!) larger. Beamtypes available are Gaussian or Constant."""
-    # Remember, the calculation region is only numpixels by numpixels
-    targetintensity = targetintensity.copy()
-    targetintensity = targetintensity / cp.max(targetintensity)
-    # Just in case we're using a highly precise target (so not delta function)
-    targetmagnification = cp.shape(targetintensity)[0] // numpixels
-    targetintensity = expand(targetintensity, magnification)
-    magnification = targetmagnification * magnification
-    slmphase = set_circlemask(expand(initialphase, magnification), numpixels *magnification)
-    inputbeam = set_circlemask(createbeam(beamtype, numpixels * magnification, sigma, mu), numpixels * magnification)
-    slmplane = join_phase_ampl(slmphase, inputbeam)
-    
-    weights=cp.ones((numpixels * magnification, numpixels*magnification))
-    weights_previous = targetintensity.copy()
-    
-    # stdinttracker = [] # For use in error calculations
-    tweezerlocation = cp.where(targetintensity == 1)
-    err_maxmindiff = []
-    err_uniformity = []
-    err_powereff = []
-
-    for _ in range(iterations):
-        startingpower = cp.sum(cp.abs(slmplane)**2)
-        fourierplane = cp.fft.fftshift(cp.fft.fft2(cp.fft.fftshift(slmplane), norm="ortho"))
-        fourierintensity = cp.square(cp.abs(fourierplane))
-        stdint = cp.divide(fourierintensity, cp.max(fourierintensity))
-
-        err_maxmindiff.append(Err_MaxMinDiff(stdint, tweezerlocation))
-        err_uniformity.append(Err_Uniformity(stdint, targetintensity))
-        err_powereff.append(Err_PowerEff(stdint, tweezerlocation))
-
-        weights = costfunction(weights, weights_previous, targetintensity, stdint, harmonicremoval, badharmonics_pixelcoords)
-        weights_previous = weights.copy()
-        ## This might be a bit confusing, but weights is now the amplitude and we re-combine it with the phase to get the next iteration
-        fourierangle = cp.angle(fourierplane)
-        fourierplane = join_phase_ampl(fourierangle, weights)
-        slmplane = cp.fft.ifftshift(cp.fft.ifft2(cp.fft.ifftshift(fourierplane), norm="ortho"))     
-        endingpower = cp.sum(cp.abs(slmplane)**2)
-        slmplane = cp.multiply(cp.divide(slmplane, endingpower), startingpower)
-        slmplane_numpixels = slmplane.copy()
-        slmplane_numpixels = cp.mean(slmplane_numpixels.reshape(numpixels, magnification, numpixels, magnification), axis=(-3,-1))
-        
-        slmphase = undiscretize_phase(discretize_phase(set_circlemask(cp.angle(slmplane_numpixels), numpixels)))
-        readout_slmphase = slmphase.copy()
-        slmplane = join_phase_ampl(expand(slmphase, magnification), inputbeam)
-    
     errors = [err_maxmindiff, err_uniformity, err_powereff]
     labels = ["MaxMinDiff","Uniformity", "Power Efficiency"]
 
